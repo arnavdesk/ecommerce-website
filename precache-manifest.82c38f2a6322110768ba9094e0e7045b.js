dac63993a3c62e62a002fdae0ce82855be93f28b
arnavdesk <arnavdesk@gmail.com>
2020-08-24 06:15:03 +0530

    Updates

---
+++ precache-manifest.82c38f2a6322110768ba9094e0e7045b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53380a8d68faffdb34d520c96f067f9a",
+    "url": "/Movie-Cherry-Ratings/ecommerce-website/index.html"
+  },
+  {
+    "revision": "6c100e92e5498eda3238",
+    "url": "/Movie-Cherry-Ratings/ecommerce-website/static/css/2.e0b71c79.chunk.css"
+  },
+  {
+    "revision": "9a6a21c0773b3314faac",
+    "url": "/Movie-Cherry-Ratings/ecommerce-website/static/css/main.d9251d04.chunk.css"
+  },
+  {
+    "revision": "6c100e92e5498eda3238",
+    "url": "/Movie-Cherry-Ratings/ecommerce-website/static/js/2.1c63a4e5.chunk.js"
+  },
+  {
+    "revision": "d4bb3d8094fe18c625c65ab2b9fef078",
+    "url": "/Movie-Cherry-Ratings/ecommerce-website/static/js/2.1c63a4e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a6a21c0773b3314faac",
+    "url": "/Movie-Cherry-Ratings/ecommerce-website/static/js/main.b9a024be.chunk.js"
+  },
+  {
+    "revision": "57ec5944da19b2e5b8f5",
+    "url": "/Movie-Cherry-Ratings/ecommerce-website/static/js/runtime-main.20f52441.js"
+  }
+]);
